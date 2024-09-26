ed18a8209b39afe69aed95b0a3b6cf01930659c7
Lwis2804 <luis.enrique.enc@gmail.com>
2024-09-26 16:50:42 -0600

    Initial Commit

---
+++ MyShop/MyShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyShopApp.swift
+//  MyShop
+//
+//  Created by LUIS GONZALEZ on 26/09/24.
+//
+
+import SwiftUI
+
+@main
+struct MyShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyShopTests/MyShopTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyShopTests.swift
+//  MyShopTests
+//
+//  Created by LUIS GONZALEZ on 26/09/24.
+//
+
+import Testing
+@testable import MyShop
+
+struct MyShopTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
